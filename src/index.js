29cc619a8bf65d6b90dc937d3e45cdaa2036297e
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2023-01-04 02:25:43 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const route = require('./Routes/route')
+const cors = require('cors')
 const {adminCreation} = require('./Controllers/userController')
 const app = express()
 app.use(express.json())
+app.use(cors()) 
 let flag = true
 mongoose.connect(
     "mongodb+srv://Manasvi29:bharat2909@cluster0.r7a9dpa.mongodb.net/order-management?retryWrites=true&w=majority",
